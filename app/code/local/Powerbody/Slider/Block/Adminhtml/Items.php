14a1cf527c3638378760671554973e2e835a9edf
mariusz <m.kwiecinski@advoxstudio.com>
2019-01-15 12:25:06 +0100

    TRAIN-191

--- app/code/local/Powerbody/Slider/Block/Adminhtml/Items.php
+++ app/code/local/Powerbody/Slider/Block/Adminhtml/Items.php
@@ -9,7 +9,6 @@ class Powerbody_Slider_Block_Adminhtml_Items extends Mage_Adminhtml_Block_Widget
         $this->_controller = 'adminhtml_items';
         $this->_blockGroup = 'powerbody_slider';
         $this->_headerText = Mage::helper('powerbody_slider')->__('Slide Item');
-        $this->_addButtonLabel = Mage::helper('powerbody_slider')->__('Add New Slide Item');
         parent::__construct();
         $this->_removeButton('add');
     }
