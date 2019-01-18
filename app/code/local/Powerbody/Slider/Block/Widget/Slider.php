73e23a44e5a486587eb815e8c7f9c132cc0005f2
mariusz <m.kwiecinski@advoxstudio.com>
2019-01-18 15:59:44 +0100

    TRAIN-193 backup

--- app/code/local/Powerbody/Slider/Block/Widget/Slider.php
+++ app/code/local/Powerbody/Slider/Block/Widget/Slider.php
@@ -2,50 +2,6 @@
 
 class Powerbody_Slider_Block_Widget_Slider extends Mage_Core_Block_Abstract implements Mage_Widget_Block_Interface
 {
-    //    protected function _construct(): string
-//    {
-//        $groupId = $this->getData('slider_list');
-//        /** @var $sliderProvider Advox_Slider_Model_Provider_Slider */
-//        $sliderProvider = Mage::getModel('advox_slider/provider_slider');
-//        /** @var $sessionModel Mage_Customer_Model_Session */
-//        $sessionModel = Mage::getSingleton('customer/session');
-//        /** @var $sliderCollection Advox_Slider_Model_Mysql4_Slider_Collection */
-//        $sliderCollection = $sliderProvider->getSlidersForGroupByCustomer(
-//            $groupId, $sessionModel->getCustomer()
-//        );
-//
-//        if (TRUE === empty($sliderCollection)) {
-//            return "";
-//        }
-//
-//        $template = $this->_getTemplatePath($groupId);
-//        $this->setData('sliderCollection', $sliderCollection)
-//            ->setTemplate($template);
-//    }
-//
-//    protected function _getTemplatePath($groupId): string
-//    {
-//        $groupModel = Mage::getModel('advox_slider/slider_group')->load($groupId);
-//
-//        /** @var $groupModel Advox_Slider_Model_Slider_Group */
-//        switch ($groupModel->getData('type')) {
-//            case Advox_Slider_Model_Slider_Group::MAIN_PAGE_SLIDER:
-//                $template = 'slider/widget/slider.phtml';
-//                break;
-//            case Advox_Slider_Model_Slider_Group::CATEGORY_SLIDER:
-//                $template = 'slider/widget/category_slider.phtml';
-//                break;
-//            case Advox_Slider_Model_Slider_Group::HTML_CONTENT_SLIDER:
-//                $template = 'slider/widget/html_content_slider.phtml';
-//                break;
-//            default:
-//                $template = 'slider/widget/slider.phtml';
-//                break;
-//        }
-//
-//        return $template;
-//    }
-
     /**
      * Produce and return widget's html output
      *
@@ -53,7 +9,36 @@ class Powerbody_Slider_Block_Widget_Slider extends Mage_Core_Block_Abstract impl
      */
     public function _toHtml(): string
     {
-        $html = "<!DOCTYPE html>
+        $groupId = $this->getData('slider_list');
+        if (isset($groupId)) {
+            $itemsCollection = Mage::getModel('powerbody_slider/item')->getCollection();
+            $itemsCollection->addFieldToFilter('group_id', ['eq' => $groupId]);
+            $itemsCollection->addFieldToFilter('display_from', ['lt' => Mage::getModel('core/date')->date('Y-m-d')]);
+            $itemsCollection->addFieldToFilter('display_To', ['gt' => Mage::getModel('core/date')->date('Y-m-d')]);
+            $itemsCollection->setOrder('sort_order', 'asc');
+        }
+        $bgImages = $itemsCollection->getColumnValues('bg_image');
+
+        return $this->slideShowRawCode($bgImages);
+    }
+
+    private function slideShowRawCode(array $bgImages)
+    {
+        $imgDiv = '';
+        $dotSpan = '';
+        for ($i = 0; $i < count($bgImages); $i++) {
+            $imgDiv .=
+            "<div class=\"mySlides fade\">
+                <div class=\"numbertext\"></div>
+                <img src=\"media/slider/" . $bgImages[$i] . "\" style=\"width:100%\">
+                <div class=\"text\"></div>
+            </div>";
+
+            $dotSpan .= "<span class=\"dot\"></span> ";
+        }
+
+        $html = "
+         <!DOCTYPE html>
 <html>
 <head>
 <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
@@ -65,7 +50,8 @@ img {vertical-align: middle;}
 
 /* Slideshow container */
 .slideshow-container {
-  max-width: 500px;
+  max-width: 300px;
+  height: 210px;
   position: relative;
   margin: auto;
 }
@@ -131,37 +117,42 @@ img {vertical-align: middle;}
 </head>
 <body>
 
-<h2>Automatic Slideshow</h2>
-<p>Change image every 2 seconds:</p>
+<div class=\"slideshow-container\">"
+            . $imgDiv .
+//<div class=\"mySlides fade\">
+//  <div class=\"numbertext\"></div>
+//  <img src=\"media/slider/images.jpeg\" style=\"width:100%\">
+//  <div class=\"text\"></div>
+//</div>
+//
+//<div class=\"mySlides fade\">
+//  <div class=\"numbertext\"></div>
+//  <img src=\"media/slider/images1.jpeg\" style=\"width:100%\">
+//  <div class=\"text\"></div>
+//</div>
+//
+//<div class=\"mySlides fade\">
+//  <div class=\"numbertext\"></div>
+//  <img src=\"media/slider/images2.jpeg\" style=\"width:100%\">
+//  <div class=\"text\"></div>
+//</div>
+//
+//<div class=\"mySlides fade\">
+//  <div class=\"numbertext\"></div>
+//  <img src=\"media/slider/images3.jpeg\" style=\"width:100%\">
+//  <div class=\"text\"></div>
+//</div>
 
-<div class=\"slideshow-container\">
-
-<div class=\"mySlides fade\">
-  <div class=\"numbertext\">1 / 3</div>
-  <img src=\"/var/www/html/trainning_mk/media/slider/images.jpeg\" style=\"width:100%\">
-  <div class=\"text\">Caption Text</div>
-</div>
-
-<div class=\"mySlides fade\">
-  <div class=\"numbertext\">2 / 3</div>
-  <img src=\"media/slider/images1.jpeg\" style=\"width:100%\">
-  <div class=\"text\">Caption Two</div>
-</div>
-
-<div class=\"mySlides fade\">
-  <div class=\"numbertext\">3 / 3</div>
-  <img src=\"/var/www/html/trainning_mk/media/slider/images2.jpeg\" style=\"width:100%\">
-  <div class=\"text\">Caption Three</div>
-</div>
-
-</div>
+            "</div>
 <br>
 
-<div style=\"text-align:center\">
-  <span class=\"dot\"></span> 
-  <span class=\"dot\"></span> 
-  <span class=\"dot\"></span> 
-</div>
+<div style=\"text-align:center\">"
+            . $dotSpan .
+//  <span class=\"dot\"></span>
+//  <span class=\"dot\"></span>
+//  <span class=\"dot\"></span>
+//  <span class=\"dot\"></span>
+            "</div>
 
 <script>
 var slideIndex = 0;
@@ -186,37 +177,10 @@ function showSlides() {
 </script>
 
 </body>
-</html> ";
+</html>
+         ";
 
         return $html;
-    }
-
-    /**
-     * Add data to the widget.
-     * Retains previous data in the widget.
-     *
-     * @param array $arr
-     * @return Mage_Widget_Block_Interface
-     */
-    public function addData(array $arr)
-    {
-
-    }
-
-    /**
-     * Overwrite data in the widget.
-     *
-     * $key can be string or array.
-     * If $key is string, the attribute value will be overwritten by $value.
-     * If $key is an array, it will overwrite all the data in the widget.
-     *
-     * @param string|array $key
-     * @param mixed $value
-     * @return Varien_Object
-     */
-    public function setData($key, $value = NULL)
-    {
-
     }
 
 }
