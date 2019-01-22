820eb849bbb225cfa4dc28548304df601535c86a
mariusz <m.kwiecinski@advoxstudio.com>
2019-01-22 15:48:11 +0100

    TRAIN-188

--- app/code/local/Powerbody/Slider/sql/powerbody_slider_setup/install-0.1.0.php
+++ app/code/local/Powerbody/Slider/sql/powerbody_slider_setup/install-0.1.0.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mariusz
- * Date: 09.01.19
- * Time: 15:11
- */
 
 $installer = $this;
 $installer->startSetup();
@@ -12,94 +6,92 @@ $installer->startSetup();
 $tableNameGr = $installer->getTable('powerbody_slider/group');
 
 // Check if the table already exists
-if ($installer->getConnection()->isTableExists($tableNameGr) != true) {
+if ($installer->getConnection()->isTableExists($tableNameGr) != TRUE) {
     $table = $installer->getConnection()
         ->newTable($tableNameGr)
         ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, 5,
-            array(
-            'unsigned' => true,
-            'nullable' => false,
-            'primary' => true,
-            'auto_increment' => true
-        ), 'Id')
+            [
+                'unsigned' => TRUE,
+                'nullable' => FALSE,
+                'primary' => TRUE,
+                'auto_increment' => TRUE,
+            ], 'Id')
         ->addColumn('name', Varien_Db_Ddl_Table::TYPE_VARCHAR, 50,
-            array(
-            'nullable' => true,
-        ), 'Name')
-        ->addColumn('created_date', Varien_Db_Ddl_Table::TYPE_DATE, null,
-            array(
-            'nullable' => true,
-        ), 'Created Date')
-        ->addColumn('updated_date', Varien_Db_Ddl_Table::TYPE_DATE, null,
-            array(
-                'nullable' => true,
-            ),
+            [
+                'nullable' => TRUE,
+            ],
+            'Name')
+        ->addColumn('created_date', Varien_Db_Ddl_Table::TYPE_DATE, NULL,
+            [
+                'nullable' => TRUE,
+            ], 'Created Date')
+        ->addColumn('updated_date', Varien_Db_Ddl_Table::TYPE_DATE, NULL,
+            [
+                'nullable' => TRUE,
+            ],
             'Updated Date');
-//        ->setComment('Powerbody Slider Group Table');
     $installer->getConnection()->createTable($table);
 }
 
 $tableNameIt = $installer->getTable('powerbody_slider/item');
 // Check if the table already exists
-if ($installer->getConnection()->isTableExists($tableNameIt) != true) {
+if ($installer->getConnection()->isTableExists($tableNameIt) != TRUE) {
     $table = $installer->getConnection()
         ->newTable($tableNameIt)
-        ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null,
-            array(
-                'unsigned' => true,
-                'nullable' => false,
-                'primary' => true,
-                'auto_increment' => true
-            ),
+        ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, NULL,
+            [
+                'unsigned' => TRUE,
+                'nullable' => FALSE,
+                'primary' => TRUE,
+                'auto_increment' => TRUE,
+            ],
             'Id'
         )
         ->addColumn('title', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255,
-            array(
-                'nullable' => false,
-            ),
+            [
+                'nullable' => FALSE,
+            ],
             'Title'
         )
-        ->addColumn('link_url', Varien_Db_Ddl_Table::TYPE_VARCHAR, null,
-            array(
-            ),
+        ->addColumn('link_url', Varien_Db_Ddl_Table::TYPE_VARCHAR, NULL,
+            [],
             'Link url'
         )
-        ->addColumn('bg_image', Varien_Db_Ddl_Table::TYPE_VARCHAR, null,
-            array(
-            ),
+        ->addColumn('bg_image', Varien_Db_Ddl_Table::TYPE_VARCHAR, NULL,
+            [],
             'Bg Image'
         )
-        ->addColumn('group_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null,
-            array(),
+        ->addColumn('group_id', Varien_Db_Ddl_Table::TYPE_INTEGER, NULL,
+            [],
             'Group Id'
         )
         ->addColumn('status', Varien_Db_Ddl_Table::TYPE_TEXT, 15,
-            array(),
+            [],
             'Status'
         )
-        ->addColumn('display_from', Varien_Db_Ddl_Table::TYPE_DATE, null,
-            array(),
+        ->addColumn('display_from', Varien_Db_Ddl_Table::TYPE_DATE, NULL,
+            [],
             'Display From'
         )
-        ->addColumn('display_to', Varien_Db_Ddl_Table::TYPE_DATE, null,
-            array(),
+        ->addColumn('display_to', Varien_Db_Ddl_Table::TYPE_DATE, NULL,
+            [],
             'Display To'
         )
-        ->addColumn('sort_order', Varien_Db_Ddl_Table::TYPE_INTEGER, null,
-            array(
-                'nullable' => false,
-                'unsigned' => true,
-            ),
+        ->addColumn('sort_order', Varien_Db_Ddl_Table::TYPE_INTEGER, NULL,
+            [
+                'nullable' => FALSE,
+                'unsigned' => TRUE,
+            ],
             'Sort Order')
-        ->addColumn('created_date', Varien_Db_Ddl_Table::TYPE_DATE, null,
-            array(
-            'nullable' => true,
-        ), 'Created Date')
-        ->addColumn('updated_date', Varien_Db_Ddl_Table::TYPE_DATE, null,
-            array(
-                'nullable' => true,
-                'unsigned' => true,
-            ),
+        ->addColumn('created_date', Varien_Db_Ddl_Table::TYPE_DATE, NULL,
+            [
+                'nullable' => TRUE,
+            ], 'Created Date')
+        ->addColumn('updated_date', Varien_Db_Ddl_Table::TYPE_DATE, NULL,
+            [
+                'nullable' => TRUE,
+                'unsigned' => TRUE,
+            ],
             'Updated Date')
         ->addForeignKey(
             $installer->getFkName(
@@ -115,6 +107,7 @@ if ($installer->getConnection()->isTableExists($tableNameIt) != true) {
             Varien_Db_Ddl_Table::ACTION_NO_ACTION);
 
     $installer->getConnection()->createTable($table);
+
 }
 
 $installer->endSetup();
